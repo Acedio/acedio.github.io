d3b8e439ea80e7ffe9fe8083b3315034e64bd86a
Josh Simmons <josh.simmons@gmail.com>
2016-02-21 20:16:47 -0800

    Updating title.

--- index.sh
+++ index.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
-echo "<html><head><title>Acedio's text and image dump</title></head><body><h1>Acedio's text and image dump</h1>"
+TITLE="Acedio's How-To Repo"
+
+echo "<html><head><title>$TITLE</title></head><body><h1>$TITLE</h1>"
 find . -maxdepth 1 -type d ! -name .\* -printf "<p><a href='%p'>%p</a></p>"
 echo "</body></html>"
