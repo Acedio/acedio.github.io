2f7c5cee4b38abc1696691fdbce0a49a518701bb
Josh Simmons <josh.simmons@gmail.com>
2016-02-21 20:13:19 -0800

    Links in <p> tags now.

--- index.sh
+++ index.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 echo "<html><head><title>Acedio's text and image dump</title></head><body><h1>Acedio's text and image dump</h1>"
-find . -maxdepth 1 -type d ! -name .\* -printf "<a href='%p'>%p</a>"
+find . -maxdepth 1 -type d ! -name .\* -printf "<p><a href='%p'>%p</a></p>"
 echo "</body></html>"
